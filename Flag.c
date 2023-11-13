d93a09974664010619af540bfc6164f54860940b
Karushi536 <Karushiabigael@gmail.com>
2023-11-14 01:47:45 +0300

    New flag.c file

--- Flag.c
+++ Flag.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-int flagSpace(int number)
+int flagSpace(int numberSin, int numberDouble)
 
 {
-    int i = 0;
+    int numberSin= 0;
     {
-        
+      putchar("%2d/n", numberSin);
+      putchar("%-2d/n", numberSin);
+      putchar("% d/n", numberSin);  
 
     }
-    
 
+    int numberDouble = 456
+    {
+        putchar("%#g/n", numberDouble);
+    }
     return (0);
 }
