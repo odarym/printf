120406e5498793c4158b3c942e68872c43f8b9cc
Karushi536 <Karushiabigael@gmail.com>
2023-11-14 01:19:05 +0300

    Tried editing theflag.c file

--- Flag.c
+++ Flag.c
@@ -3,9 +3,11 @@
 int flagSpace(int number)
 
 {
-    int a = 1234;/*initialize number a*/
-    double b = 1234.0;/*initialize b*/
+    int a = 1234;
+    double b = 1234.0;
 
-    putchar("%+2d/n %-2d/n % 2d/n", a );
-    putchar("%#g/n", b );
+    putchar("%+2d/n %-2d/n % 2d/n", a);
+    putchar("%#g/n", b);
+
+    return(0);
 }
