24ed28e3305951c42c810160d392d8d55370274f
Karushi536 <Karushiabigael@gmail.com>
2023-11-14 01:22:05 +0300

    Edited the flag.c file but still having issues

--- Flag.c
+++ Flag.c
@@ -6,8 +6,8 @@ int flagSpace(int number)
     int a = 1234;
     double b = 1234.0;
 
-    putchar("%+2d/n %-2d/n % 2d/n", a);
-    putchar("%#g/n", b);
+    putchar("%+2d/n %-2d/n % 2d/n", a );
+    putchar("%#g/n", b );
 
     return(0);
 }
