2c143a0accc18db4764d37bb22656ec50f310d1e
Karushi536 <Karushiabigael@gmail.com>
2023-11-14 01:11:48 +0300

    Created a flag.c file

--- Flag.c
+++ Flag.c
@@ -6,6 +6,6 @@ int flagSpace(int number)
     int a = 1234;/*initialize number a*/
     double b = 1234.0;/*initialize b*/
 
-    putchar("%+2d/n %-2d/n % 2d/n", a );
-    putchar("%#g/n", b );
+    putchar("%+2d/n %-2d/n % 2d/n", a);
+    putchar("%#g/n", b);
 }
