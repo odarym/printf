7b058d9f3e07a0f8a866a4bb539abb5cd85c2b76
Karushi536 <Karushiabigael@gmail.com>
2023-11-15 02:13:53 +0300

    still editing the flag file

--- PrintfFlags.c
+++ PrintfFlags.c
@@ -18,6 +18,7 @@ int PrintfFlags(char flag, va_list arguments)
 	{
 		case '+':
 			count++;
+			
 			/*Print the flag for now. Checking if flag was received correctly*/
 			Putchar(flag);
 			Putchar(specifier);
