ff46ce9a5a4210c0c2611fdcbcd27b7936661cef
MosesOdary <mosesodary101@gmail.com>
2023-11-14 14:54:24 +0300

    Modified the test cases

--- main.c
+++ main.c
@@ -29,20 +29,26 @@ int main(void)
 	printf("Unsigned octal:[%o]\n", ui);
 	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	/*
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
+	*/
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
+	len = _printf("%r\n", "Unknown");
+	_printf("Len:[%d]\n", len);
 	printf("Unknown:[%r]\n");
+	_printf("%b\n", 8);
+	_printf("%b\n", 7);
+	_printf("%b\n", 9);
 
-	/*Flags secifiers tests*/
+	/*Flags secifiers tests
 	_printf("\nFlags specifiers tests\n\n");
 	_printf("Preceding with blanks: %10d \n", 1977);
 	printf("Preceding with blanks: %10d \n", 1977);
@@ -54,7 +60,7 @@ int main(void)
 	printf("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
 	_printf("Width trick: %*d \n", 5, 10);
 	printf("Width trick: %*d \n", 5, 10);
-	/**/
+	*/
 	
 	return (0);
 }
