00bee94ca07330fb9b525a2a2f0a92b8a5c6755a
MosesOdary <mosesodary101@gmail.com>
2023-11-15 15:07:32 +0300

    Fixed some printf edge cases

--- PrintfFlags.c
+++ PrintfFlags.c
@@ -13,39 +13,38 @@ int PrintfFlags(char flag, va_list arguments)
 	char specifier = va_arg(arguments, int);
 	int count = 0;
 
-    switch (flag)
-    {
-        case '+':
-            count++;
-            Putchar(flag);
-            Putchar(specifier);
-            /*Handle behavior for '+' flag in different states if needed*/
-            break;
-        case '-':
-            count++;
-            Putchar(flag);
-            Putchar(specifier);
-            /*Handle behavior for '-' flag in different states if needed*/
-            break;
-        case ' ':
-            count++;
-            Putchar(flag);
-            Putchar(specifier);
-            /*Handle behavior for ' ' flag in different states if needed*/
-            break;
-        case '#':
-            count++;
-            Putchar(flag);
-            Putchar(specifier);
-            /*Handle behavior for '#' flag in different states if needed*/
-            break;
-        case '0':
-            count++;
-            Putchar(flag);
-            Putchar(specifier);
-            /*Handle behavior for '0' flag in different states if needed*/
-            break;
-    }
+	switch (flag)
+	{
+		case '+':
+			Putchar(flag);
+			Putchar(specifier);
+			if (specifier == 'd' || specifier == 'i')
+			{
+				int number = va_arg(arguments, int);
 
+				if (number >= 0)
+				{
+					Putchar('+');
+					count++;
+				}
+			}
+			break;
+		case '-':
+			/*Handle behavior for '-' flag in different states if needed*/
+			break;
+		case ' ':
+			/*Handle behavior for ' ' flag in different states if needed*/
+			break;
+		case '#':
+			Putchar(flag);
+			Putchar(specifier);
+			/*Handle behavior for '#' flag in different states if needed*/
+			break;
+		case '0':
+			Putchar(flag);
+			Putchar(specifier);
+			/*Handle behavior for '0' flag in different states if needed*/
+			break;
+	}
 	return (count);
 }
--- _printf.h
+++ _printf.h
@@ -1,14 +1,33 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
 #include "main.h"
 
+/**
+ * enum PrintfState - Contains the different states that
+ * _printf can be in
+*/
 typedef enum
 {
-	NORMAL = 0, LENGTH, SHORT, LONG, SPEC
+	NORMAL = 0, FLAG, LENGTH, SHORT, LONG, SPEC
 } PrintfState;
 
+/**
+ * enum PrintfLengthState - Contains the different length states that
+ * _printf can be in
+*/
 typedef enum
 {
-	LEN_DEFAULT = 0, LEN_SHORT_SHORT, LEN_SHORT, LEN_LONG, LEN_LONG_LONG
+	LEN_DEFAULT = 10, LEN_SHORT_SHORT, LEN_SHORT, LEN_LONG, LEN_LONG_LONG
 } PrintfLengthState;
 
 uint8_t PrintfNum(uint64_t arg, uint8_t length,
 					bool sign, uint8_t base, bool hexUpper);
+
+void PrintfSpecifierParser(int *countPtr, uint16_t i, const char *specifier,
+	va_list arguments, uint8_t base, bool sign,
+	PrintfLengthState length, bool hexUpper);
+
+
+#endif
+
