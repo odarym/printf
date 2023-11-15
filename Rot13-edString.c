739ffbb3fac2724d2e8c9aebd8ec3c2dbfd3aec3
MosesOdary <mosesodary101@gmail.com>
2023-11-15 04:30:17 +0300

    Prints a Rot13'ed string

---
+++ Rot13-edString.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * PutsReverse - Print string in reverse
+ *
+ * @string: The string being printed in reverse
+ * Return:void
+*/
+
+int PutsRot13(char *string)
+{
+	int stringLength = strlen(string);
+	int count = 0;
+
+	count = stringLength;
+	
+	/*Change string to Rot13 and print it*/
+	/*Update count if needed*/
+
+	return (count);
+}
+
