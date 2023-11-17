afbab8d71158a78cab2212407f10045d43287d30
Karushi536 <Karushiabigael@gmail.com>
2023-11-18 01:52:44 +0300

    Added values to it

--- Non-custom.c
+++ Non-custom.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * Main - function to demonstrate printf with precision.
  * Return: 0 on successful execution.
  */
-int nonCustom(int intValue, int  floatValue, int stringValue) 
+int nonCustom(int intValue, int  floatValue, char stringValue) 
 {
     int intValue = 12345;
     float floatValue = 123.456789;
