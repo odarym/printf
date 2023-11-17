7ec4e9ec2338ad95fdd39405deb49bc1cde4f9b8
Karushi536 <Karushiabigael@gmail.com>
2023-11-18 01:58:34 +0300

    Edited two files

--- Non-custom.c
+++ Non-custom.c
@@ -5,7 +5,7 @@
  * Main - function to demonstrate printf with precision.
  * Return: 0 on successful execution.
  */
-int nonCustom(int intValue, int  floatValue, char stringValue) 
+int nonCustom(int intValue, int  floatValue, const char *stringValue) 
 {
     int intValue = 12345;
     float floatValue = 123.456789;
--- main.h
+++ main.h
@@ -17,7 +17,7 @@ int DecimalToBinary(uint32_t decimalNum);
 int PutsCustom(char *string);
 int PutsReverse(char *string);
 int PutsRot13(char *string);
-int nonCustom(int intValue, int  floatValue, int stringValue);
+int nonCustom(int intValue, int  floatValue, const char *stringValue);
 
 
 
