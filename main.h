73253c8447a4a10e0dcd1c4289da012917d19eb9
Karushi536 <Karushiabigael@gmail.com>
2023-11-18 01:59:31 +0300

    Reverted main.h to its normal form

--- main.h
+++ main.h
@@ -17,7 +17,6 @@ int DecimalToBinary(uint32_t decimalNum);
 int PutsCustom(char *string);
 int PutsReverse(char *string);
 int PutsRot13(char *string);
-int nonCustom(int intValue, int  floatValue, const char *stringValue);
 
 
 
