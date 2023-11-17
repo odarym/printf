a233661d7d75257fa9cb667fff0072ba796d3ce5
Karushi536 <Karushiabigael@gmail.com>
2023-11-18 01:26:53 +0300

    created a new file for nonCustomSpecifiers

---
+++ Non-custom.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * @brief Main function to demonstrate printf with precision.
+ * @return 0 on successful execution.
+ */
+int main(void) 
+{
+    int intValue = 12345;
+    float floatValue = 123.456789;
+    char stringValue[] = "Hello, World!";
+
+    printf("%.5d\n", intValue); 
+    printf("%.2f\n", floatValue); 
+    printf("%.7s\n", stringValue);
+
+    return (0);
+}
